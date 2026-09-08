d8f5f1b0d55ff34d03858b03d3dcf254c859f442
Liwenyu123-spec <319456867+Liwenyu123-spec@users.noreply.github.com>
2026-09-08 14:35:18 +0800

    auto: sync 2026-09-08 14:35:18

--- 908.py
+++ 908.py
@@ -1,1 +1,19 @@
 
+import os
+from openai import OpenAI
+
+print(os.getenv("DEEPSEEK_API_KEY"))
+
+client = OpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com"
+)
+
+response = client.chat.completions.create(
+    model="deepseek-v4-flash",
+    messages=[
+        {"role":"user","content":"你是谁？"}
+    ]
+)
+print(response.choices[0].message.content)
+
