1d11e588bca681309e8dbdabec5d0ddcc41d595f
Liwenyu123-spec <319456867+Liwenyu123-spec@users.noreply.github.com>
2026-09-12 11:02:15 +0800

    auto: sync 2026-09-12 11:02:14

--- 912.py
+++ 912.py
@@ -102,7 +102,7 @@ upper_bound = 100
 # ==========================================
 
 # 【挖空 ①】
-abnormal_rows = ((df[['PH值','浊度']]<0|[['PH值','浊度']]>100).any(axis=1))
+abnormal_rows = ((df[['PH值','浊度']]<0)|([['PH值','浊度']]>100).any(axis=1))
 
 print("\n=== 检测到异常数据行 ===")
 print(abnormal_rows)
